27888085f6690b95d99a0bf6244614f8db24eb55
xuhuang <xuhuang@theapollobox.com.cn>
2019-06-20 17:34:02 +0800

    smart contract folder added

---
+++ Smart Contract/README.md
@@ -0,0 +1,12 @@
+####connect to a network:
+
+```shell
+npm run connect <network name>
+```
+
+* connect to RSK testnet
+  ```
+  npm run connect testnet
+  ```
+
+
---
+++ Smart Contract/truffle-config.js
@@ -0,0 +1,44 @@
+/**
+ * Use this file to configure your truffle project. It's seeded with some
+ * common settings for different networks and features like migrations,
+ * compilation and testing. Uncomment the ones you need or modify
+ * them to suit your project as necessary.
+ *
+ * More information about configuration can be found at:
+ *
+ * truffleframework.com/docs/advanced/configuration
+ *
+ * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura accounts
+ * are available for free at: infura.io/register.
+ *
+ * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. If you're publishing your code to GitHub make sure you load this
+ * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ *
+ */
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const mnemonic = 'view blood flavor endless truth lobster seed ice anxiety guide pulp laundry';
+
+var publicNode = 'https://public-node.testnet.rsk.co:443'
+
+module.exports = {
+  networks: {
+    testnet: {
+      provider: () =>
+        new HDWalletProvider(mnemonic, publicNode),
+      network_id: '*',
+      gas: 2500000,
+      gasPrice: 183000
+    }
+  },
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.5.0",
+      evmVersion: "byzantium"
+    }
+  }
+}
