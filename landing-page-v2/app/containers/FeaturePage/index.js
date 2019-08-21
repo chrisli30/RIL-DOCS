4c8fd4457c7d39f3503319e86891fca817387609
xuhuang <xuhuang@theapollobox.com.cn>
2019-08-21 09:13:27 +0800

    fixed h1 can not be resolved

--- landing-page-v2/app/containers/FeaturePage/index.js
+++ landing-page-v2/app/containers/FeaturePage/index.js
@@ -7,7 +7,6 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 
-import H1 from 'components/H1';
 import messages from './messages';
 import List from './List';
 import ListItem from './ListItem';
